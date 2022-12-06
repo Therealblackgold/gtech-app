99f3280eca6fdcfdc1af6be0b0e8222569153439
Therealblackgold <gautancholo@gmail.coml>
2022-12-07 01:12:37 +0200

    login details

--- src/components/Public.jsx
+++ src/components/Public.jsx
@@ -9,15 +9,30 @@ export const Public = () => {
     <>
       <PublicHeader />
       <main className="public__main">
-        <p>This is an introduction</p>
+        {/* admin login details, this is only here for code reviewer testing this application */}
+        <div className="login-details">
+          <p style={{ color: "rgb(202, 26, 26" }}>
+            Only managers and admin users can create employee users, for testing
+            please use details below to login!!!
+          </p>
+          <span>username: admin</span>
+          <span>password: 123456</span>
+        </div>
+        {/* employee login details, this is only here for code reviewer testing this application  */}
+        <div className="login-details">
+          <p style={{ color: "rgb(202, 26, 26" }}>
+            To login as an employee use login details below or create an
+            employee user logged in as an admin user!!!
+          </p>
+          <span>username: username</span>
+          <span>password: 123456</span>
+        </div>
         <address className="public__addr">
           G Tech.COM Repairs <br />
           23 Forest Hills Drive <br />
           Foo City, Cape Town 12345 <br />
           <a href="tell:+55555555555555">(555) 555-5555</a>
         </address>
-        <br />
-        <p>Owner: Kate Wilson</p>
       </main>
       <PublicFooter />
     </>
